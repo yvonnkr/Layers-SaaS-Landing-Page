208c3af515b17416873e138e507d3f019ec84062
yvonnkr <yvonnkr86@gmail.com>
2024-11-28 15:43:18 +0000

    implement footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,28 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row md:justify-between items-center gap-6">
+                    <div className="flex justify-center">
+                        <Image src={logoImage} alt="layrs logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    key={link.label}
+                                    href={link.href}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
