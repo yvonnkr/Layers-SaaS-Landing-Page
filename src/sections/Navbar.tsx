7e6acf1bfec1a0df7272d06bd1d1d28674932b42
yvonnkr <yvonnkr86@gmail.com>
2024-11-28 17:55:49 +0000

    add navbar section animation

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
 import logoImage from "@/assets/images/logo.svg";
 import Button from "@/components/Button";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -10,61 +14,130 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <section className="py-4 lg:py-8">
-            <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
-                    <div className="">
-                        <Image
-                            src={logoImage}
-                            alt="layers logo"
-                            className="h-9 md:h-auto w-auto"
-                        />
-                    </div>
+        <>
+            <section className="py-4 lg:py-8 fixed w-full top-0 z-50">
+                <div className="container max-w-5xl">
+                    <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur">
+                        <div className="grid grid-cols-2 lg:grid-cols-3  p-2 px-4 md:pr-2 items-center ">
+                            <div className="">
+                                <Image
+                                    src={logoImage}
+                                    alt="layers logo"
+                                    className="h-9 md:h-auto w-auto"
+                                />
+                            </div>
 
-                    <div className="hidden lg:flex justify-center items-center">
-                        <nav className="flex gap-6 font-medium">
-                            {navLinks.map((link) => (
-                                <a href={link.href} key={link.label}>
-                                    {link.label}
-                                </a>
-                            ))}
-                        </nav>
-                    </div>
+                            <div className="hidden lg:flex justify-center items-center">
+                                <nav className="flex gap-6 font-medium">
+                                    {navLinks.map((link) => (
+                                        <a href={link.href} key={link.label}>
+                                            {link.label}
+                                        </a>
+                                    ))}
+                                </nav>
+                            </div>
 
-                    <div className="flex justify-end gap-4">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="feather feather-menu md:hidden"
-                        >
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
-                        </svg>
+                            <div className="flex justify-end gap-4">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="feather feather-menu md:hidden"
+                                    onClick={() => setIsOpen(!isOpen)}
+                                >
+                                    <line
+                                        x1="3"
+                                        y1="6"
+                                        x2="21"
+                                        y2="6"
+                                        className={twMerge(
+                                            "origin-left transition",
+                                            isOpen && "rotate-45 -translate-y-1"
+                                        )}
+                                    ></line>
+                                    <line
+                                        x1="3"
+                                        y1="12"
+                                        x2="21"
+                                        y2="12"
+                                        className={twMerge(
+                                            "transition",
+                                            isOpen && "opacity-0"
+                                        )}
+                                    ></line>
+                                    <line
+                                        x1="3"
+                                        y1="18"
+                                        x2="21"
+                                        y2="18"
+                                        className={twMerge(
+                                            "origin-left transition",
+                                            isOpen && "-rotate-45 translate-y-1"
+                                        )}
+                                    ></line>
+                                </svg>
 
-                        <Button
-                            variant="secondary"
-                            className="hidden md:inline-flex items-center"
-                        >
-                            Log In
-                        </Button>
-                        <Button
-                            variant="primary"
-                            className="hidden md:inline-flex items-center"
-                        >
-                            Sign Up
-                        </Button>
+                                <Button
+                                    variant="secondary"
+                                    className="hidden md:inline-flex items-center"
+                                >
+                                    Log In
+                                </Button>
+                                <Button
+                                    variant="primary"
+                                    className="hidden md:inline-flex items-center"
+                                >
+                                    Sign Up
+                                </Button>
+                            </div>
+                        </div>
+                        <AnimatePresence>
+                            {isOpen && (
+                                <motion.div
+                                    className=" overflow-hidden"
+                                    initial={{ height: 0 }}
+                                    animate={{ height: "auto" }}
+                                    exit={{ height: 0 }}
+                                    transition={{
+                                        duration: 0.5,
+                                        ease: "easeIn",
+                                    }}
+                                >
+                                    <div className="flex flex-col items-center gap-4 py-4">
+                                        {navLinks.map((link) => (
+                                            <a
+                                                key={link.label}
+                                                href={link.href}
+                                                className=""
+                                            >
+                                                {link.label}
+                                            </a>
+                                        ))}
+
+                                        <Button variant="secondary">
+                                            Log In
+                                        </Button>
+                                        <Button variant="primary">
+                                            Sign Up
+                                        </Button>
+                                    </div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            <div className="pb-[86px] md:pb-[98px] lg:pb-[130px]"></div>
+        </>
     );
 }
